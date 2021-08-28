d48b22f4c6d9351bed27427065635f1f3cd328d3
danlo924 <daniel.r.wilde@gmail.com>
2021-08-27 22:11:45 -0700

    latest updates

--- Python_OOP_MiniProject/console.py
+++ Python_OOP_MiniProject/console.py
@@ -1,12 +1,10 @@
-import model      ##why doesn't this work?
-# from model import Customer
+import model as m
 
-print("Hello! Welcome to the Bank!")
+print("\nHello! Welcome to Dan's Bank!")
 
-cust_input = input("Do you have an account with us? (Y/N): ")
+cust_input = input("\nDo you have an account with us? (Y/N): ")
 
 # if cust_input == "N":  ## create new customer
 
-
-cust1 = Customer("Dan","Wilde","Skyview St","123456789")
-print(cust1.first_name)
+# cust1 = m.Customer("Dan","Wilde","Skyview St","123456789")
+# print(cust1.first_name)
