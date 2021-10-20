21b0ad1e62fdcf9b4b4f04651adb598d3fe3ea62
danlo924 <daniel.r.wilde@gmail.com>
2021-10-20 11:14:47 -0700

    Capstone Step 3: Data Collection

---
+++ Capstone/DataCollection/getSandPData.py
@@ -0,0 +1,80 @@
+import os, ssl
+import pandas as pd
+from sqlalchemy import create_engine
+import yfinance as yf
+
+# BYPASS CERTIFICATE FOR NOW
+if (not os.environ.get('PYTHONHTTPSVERIFY', '') and getattr(ssl, '_create_unverified_context', None)):
+    ssl._create_default_https_context = ssl._create_unverified_context
+
+# CREATE ENGINE FOR MYSQL INVESTING DATABASE
+investing_db_engine = create_engine(
+        "mysql+pymysql://{user}:{pw}@{host}/{db}".format(
+            host='127.0.0.1'
+            ,db='investing'
+            ,user='root'
+            ,pw='<pw_here>'
+        )
+    )
+
+# CREATE A NEW CONNECTION TO THE DB
+investing_db_conn = investing_db_engine.raw_connection()
+
+# GET FULL ACTIVE LIST OF S&P 500 COMPANIES, ALONG WITH COMPANIES ADDED / REMOVED IN THE PAST 20 YEARS
+SandP500List=pd.read_html('https://en.wikipedia.org/wiki/List_of_S%26P_500_companies')
+sp_curr = SandP500List[0]
+sp_hist = SandP500List[1]
+
+# IMPORT CURRENT AND HISTORICAL S&P LISTS TO STAGING TABLES
+sp_curr.to_sql(con=investing_db_engine, name='_stg_sp_current', if_exists='replace')
+sp_hist.to_sql(con=investing_db_engine, name='_stg_sp_history', if_exists='replace')
+
+# CALL sp_refresh_companies STORED PROC TO REFRESH S&P LIST
+try:
+    cursor = investing_db_conn.cursor()
+    cursor.callproc('sp_refresh_companies')
+    results = list(cursor.fetchall())
+    cursor.close()
+    investing_db_conn.commit()
+except Exception as error:
+    print(error)
+
+# ITERATE THROUGH STOCKS AND GET PRICE HISTORY
+sql = '''SELECT c.Ticker
+    FROM companies c
+    WHERE NOT EXISTS 
+    (
+        SELECT 1
+        FROM companies c2
+            INNER JOIN prices p ON c2.CompanyID = p.CompanyID
+        WHERE c2.CompanyID = c.CompanyID
+        LIMIT 1
+    )
+    ORDER BY Ticker
+    LIMIT 10;
+    '''
+
+try:
+    cursor = investing_db_conn.cursor()
+    cursor.execute(sql)
+    df_results = pd.DataFrame(cursor.fetchall(), columns=['Ticker'])
+    cursor.close()
+    investing_db_conn.commit()     
+    for index, row in df_results.iterrows():
+        ticker = str(row['Ticker'])
+        print('Get the price history for: ' + ticker)
+        price_hist = yf.Ticker(ticker)
+        price_hist_df = price_hist.history(period='max')
+        price_hist_df.to_sql(con=investing_db_engine, name='_stg_price_hist', if_exists='replace')
+        try:
+            cursor_pr = investing_db_conn.cursor()
+            cursor_pr.callproc('sp_refresh_prices', args=[ticker,])
+            results = list(cursor_pr.fetchall())
+            cursor_pr.close()
+            investing_db_conn.commit()
+        except Exception as error:
+            print(error)
+except Exception as error:
+    print(error)
+
+investing_db_conn.close()
---
+++ Capstone/InvestingDB/investing_db_schema.sql
@@ -0,0 +1,214 @@
+-- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+-- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
+-- -----------------------------------------------------
+-- Schema investing
+-- -----------------------------------------------------
+
+-- -----------------------------------------------------
+-- Schema investing
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `investing` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci ;
+USE `investing` ;
+
+-- -----------------------------------------------------
+-- Table `investing`.`_stg_price_hist`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `investing`.`_stg_price_hist` (
+  `Date` DATETIME NULL DEFAULT NULL,
+  `Open` DOUBLE NULL DEFAULT NULL,
+  `High` DOUBLE NULL DEFAULT NULL,
+  `Low` DOUBLE NULL DEFAULT NULL,
+  `Close` DOUBLE NULL DEFAULT NULL,
+  `Volume` BIGINT NULL DEFAULT NULL,
+  `Dividends` DOUBLE NULL DEFAULT NULL,
+  `Stock Splits` DOUBLE NULL DEFAULT NULL,
+  INDEX `ix__stg_price_hist_Date` (`Date` ASC) VISIBLE)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
+-- Table `investing`.`_stg_sp_current`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `investing`.`_stg_sp_current` (
+  `index` BIGINT NULL DEFAULT NULL,
+  `Symbol` TEXT NULL DEFAULT NULL,
+  `Security` TEXT NULL DEFAULT NULL,
+  `SEC filings` TEXT NULL DEFAULT NULL,
+  `GICS Sector` TEXT NULL DEFAULT NULL,
+  `GICS Sub-Industry` TEXT NULL DEFAULT NULL,
+  `Headquarters Location` TEXT NULL DEFAULT NULL,
+  `Date first added` TEXT NULL DEFAULT NULL,
+  `CIK` BIGINT NULL DEFAULT NULL,
+  `Founded` TEXT NULL DEFAULT NULL,
+  INDEX `ix__stg_sp_current_index` (`index` ASC) VISIBLE)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
+-- Table `investing`.`_stg_sp_history`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `investing`.`_stg_sp_history` (
+  `index` BIGINT NULL DEFAULT NULL,
+  `('Date', 'Date')` TEXT NULL DEFAULT NULL,
+  `('Added', 'Ticker')` TEXT NULL DEFAULT NULL,
+  `('Added', 'Security')` TEXT NULL DEFAULT NULL,
+  `('Removed', 'Ticker')` TEXT NULL DEFAULT NULL,
+  `('Removed', 'Security')` TEXT NULL DEFAULT NULL,
+  `('Reason', 'Reason')` TEXT NULL DEFAULT NULL,
+  INDEX `ix__stg_sp_history_index` (`index` ASC) VISIBLE)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
+-- Table `investing`.`companies`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `investing`.`companies` (
+  `CompanyID` INT NOT NULL AUTO_INCREMENT,
+  `Name` VARCHAR(100) NOT NULL,
+  `Description` TEXT NULL DEFAULT NULL,
+  `Ticker` CHAR(15) NOT NULL,
+  `Sector` VARCHAR(100) NULL DEFAULT NULL,
+  `Industry` VARCHAR(100) NULL DEFAULT NULL,
+  `Founded` INT NULL DEFAULT NULL,
+  `SPAddDate` DATETIME NULL DEFAULT NULL,
+  `SPRemDate` DATETIME NULL DEFAULT NULL,
+  `IsIndex` BIT(1) NOT NULL DEFAULT b'0',
+  `UpdatedDate` DATETIME NULL DEFAULT NULL,
+  PRIMARY KEY (`CompanyID`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 506
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
+-- Table `investing`.`prices`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `investing`.`prices` (
+  `CompanyID` INT NOT NULL,
+  `Date` DATETIME NOT NULL,
+  `Open` FLOAT NULL DEFAULT NULL,
+  `High` FLOAT NULL DEFAULT NULL,
+  `Low` FLOAT NULL DEFAULT NULL,
+  `Close` FLOAT NULL DEFAULT NULL,
+  `Volume` BIGINT NULL DEFAULT NULL,
+  `UpdatedDate` DATETIME NULL DEFAULT NULL,
+  PRIMARY KEY (`CompanyID`, `Date`),
+  CONSTRAINT `prices_ibfk_1`
+    FOREIGN KEY (`CompanyID`)
+    REFERENCES `investing`.`companies` (`CompanyID`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+USE `investing` ;
+
+-- -----------------------------------------------------
+-- procedure sp_refresh_companies
+-- -----------------------------------------------------
+
+DELIMITER $$
+USE `investing`$$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_refresh_companies`()
+BEGIN
+
+	INSERT INTO companies(Name, Ticker, Sector, Industry, Founded, SPAddDate, IsIndex, UpdatedDate)
+    SELECT spc.`Security`
+		,spc.Symbol
+        ,spc.`GICS Sector`
+        ,spc.`GICS Sub-Industry`
+        ,substring_index(substring_INDEX(spc.Founded, ' ', 1), '/', 1)
+        ,substring_INDEX(spc.`Date first added`, ' (', 1)
+        ,0
+        ,sysdate(3)
+    FROM _stg_sp_current spc
+		LEFT OUTER JOIN companies c ON spc.Symbol = c.Ticker
+	WHERE c.Ticker IS NULL;
+    
+    UPDATE companies c
+    INNER JOIN _stg_sp_current spc ON spc.Symbol = c.Ticker
+    SET c.Name = spc.`Security`
+		,c.Sector = spc.`GICS Sector`
+        ,c.Industry = spc.`GICS Sub-Industry`
+        ,c.Founded = substring_index(substring_INDEX(spc.Founded, ' ', 1), '/', 1)
+        ,c.SPAddDate = substring_INDEX(spc.`Date first added`, ' (', 1)
+        ,c.IsIndex = 0
+        ,c.UpdatedDate = sysdate(3);
+        
+END$$
+
+DELIMITER ;
+
+-- -----------------------------------------------------
+-- procedure sp_refresh_prices
+-- -----------------------------------------------------
+
+DELIMITER $$
+USE `investing`$$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_refresh_prices`(
+	IN StockTicker CHAR(15)
+    )
+BEGIN
+	
+    DECLARE CompanyID_filter INT;
+    SET CompanyID_filter = (SELECT CompanyID FROM Companies WHERE Ticker = StockTicker);
+
+	INSERT INTO Prices
+    (
+		CompanyID
+        ,Date
+        ,Open
+        ,High
+        ,Low
+        ,Close
+        ,Volume
+        ,UpdatedDate
+	)
+    SELECT 
+		c.CompanyID
+        ,ph.Date
+        ,ph.Open
+        ,ph.High
+        ,ph.Low
+        ,ph.Close
+        ,ph.Volume
+        ,sysdate(3)
+	FROM _stg_price_hist ph
+		INNER JOIN companies c ON 1=1
+		LEFT OUTER JOIN Prices p ON c.CompanyID = p.CompanyID
+			AND p.Date = ph.Date
+	WHERE 
+		c.CompanyID = CompanyID_filter
+        AND p.Date IS NULL;
+        
+    UPDATE prices p
+    INNER JOIN _stg_price_hist ph ON p.Date = ph.Date
+		AND p.CompanyID = CompanyID_filter
+    SET p.Open = ph.Open
+		,p.High = ph.High
+        ,p.Low = ph.Low
+        ,p.Close = ph.Close
+        ,p.Volume = ph.Volume
+        ,p.UpdatedDate = sysdate(3)
+	WHERE p.CompanyID = CompanyID_filter;
+    
+END$$
+
+DELIMITER ;
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
