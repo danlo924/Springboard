78047421d848c5a6e9fd3c58d81414e2ee845430
danlo924 <daniel.r.wilde@gmail.com>
2021-12-04 21:31:15 -0800

    final pipeline.py updates

--- Capstone/DataCollection/pipeline.py
+++ Capstone/DataCollection/pipeline.py
@@ -1,22 +1,69 @@
 import ssl
-import datetime
 import logging
 import json
-from pandas.core.frame import DataFrame
 import requests
 import pandas as pd
 import yfinance as yf
 import urllib.request as req
+from datetime import datetime
 from sqlalchemy import create_engine
 from secrets import secrets
 
 logging.basicConfig(
-    filename='ignore\logs\pipeline.log',
+    filename=datetime.now().strftime('ignore\logs\pipeline_%Y_%m_%d_%H_%M_%S.log'),
     filemode='a',
     format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
     datefmt='%H:%M:%S',
     level=logging.DEBUG
 )
+
+def log_message(msg, show=False):
+    logging.debug(msg)
+    if show==True:
+        print(msg)
+
+def log_df_details(df_name, df):
+    try:
+        log_message(df_name + ' DataFrame Loaded: ' + str(df.shape[0]) + ' Rows, ' + str(df.shape[1]) + ' Columns.')
+    except Exception as error:
+        log_message(error, show=True)    
+
+def get_price_refresh_sql(max_tickers, lookback_window):
+    sql = '''
+        SELECT REPLACE(c.Ticker,'.','-') AS Ticker
+        FROM companies c
+        WHERE NOT EXISTS 
+        (
+            SELECT 1
+            FROM companies c2
+                INNER JOIN prices p ON c2.CompanyID = p.CompanyID
+            WHERE c2.CompanyID = c.CompanyID
+                AND p.Date > date_sub(now(), interval ''' + str(lookback_window) + ''' day)
+            LIMIT 1
+        )
+        ORDER BY Ticker
+        LIMIT ''' + str(max_tickers) + ''';'''
+    log_message('SQL for Price Refresh: ' + sql)
+    return sql
+
+def get_div_refresh_sql(max_tickers, lookback_window):
+    sql = '''
+        SELECT REPLACE(c.Ticker,'.','-') AS Ticker
+        FROM companies c
+        WHERE c.HasDividend = 1
+            AND NOT EXISTS 
+            (
+                SELECT 1
+                FROM companies c2
+                    INNER JOIN dividends d ON c2.CompanyID = d.CompanyID
+                WHERE c2.CompanyID = c.CompanyID
+                    AND d.ExDivDate > date_sub(now(), interval ''' + str(lookback_window) + ''' day)
+                LIMIT 1
+            )
+        ORDER BY Ticker
+        LIMIT ''' + str(max_tickers) + ''';'''
+    log_message('SQL for Dividends Refresh: ' + sql)
+    return sql            
 
 # PIPELINE CLASS DEFINITION
 class Pipeline:
@@ -29,52 +76,26 @@ class Pipeline:
         self.log_pipeline_details()
 
     def log_pipeline_details(self):
-        logging.debug('\nPipeline Run Details (' + str(datetime.datetime.now()) + '):'
-        + '\nDatabase connection info: ' + self.db_url  
-        + '\nCompanies data url: ' + self.companies_data_url 
-        + '\nDividend data API url: ' + self.div_data_url
-        + '\nSeeking Aplha API key: ' + self.sa_api_key)
-
-    def log_df_details(self, df_name, df):
-        try:
-            logging.debug(df_name + ' DataFrame Loaded: ' + str(df.shape[0]) + ' Rows, ' + str(df.shape[1]) + ' Columns.')
-        except Exception as error:
-            logging.debug(error)
-            print(error)   
+        log_message(
+            '\nPipeline Run Details (' + str(datetime.now()) + '):'
+            + '\nDatabase connection info: ' + self.db_url  
+            + '\nCompanies data url: ' + self.companies_data_url 
+            + '\nDividend data API url: ' + self.div_data_url
+            + '\nSeeking Aplha API key: ' + self.sa_api_key
+            )
 
     def create_db_conn(self):
         try:
             self.db_engine = create_engine(self.db_url)
             self.db_conn = self.db_engine.raw_connection()
         except Exception as error:
-            logging.debug(error)
-            print(error)
-
-
-    def get_sandp_companies_list(self):
-        try:
-            url = self.companies_data_url
-            context = ssl._create_unverified_context()
-            response = req.urlopen(url, context=context)
-            html = response.read()
-            sandp_companies = pd.read_html(html)
-            sp_curr = sandp_companies[0]
-            self.log_df_details('S&P Current Company List', sp_curr)
-            sp_hist = sandp_companies[1]
-            self.log_df_details('S&P Historical Company List', sp_hist)
-            self.load_df_to_stg(sp_curr, '_stg_sp_current')
-            self.load_df_to_stg(sp_hist, '_stg_sp_history')
-            self.call_proc('sp_refresh_companies')   
-        except Exception as error:
-            logging.debug(error)
-            print(error)               
+            log_message(error, show=True)
 
     def load_df_to_stg(self, df, stg_tbl_name):
         try:
             df.to_sql(con=self.db_engine, name=stg_tbl_name, if_exists='replace')
         except Exception as error:
-            logging.debug(error)
-            print(error)            
+            log_message(error, show=True)           
 
     def call_proc(self, proc_name, args=None):
         try:
@@ -87,46 +108,24 @@ class Pipeline:
             cursor.close()
             self.db_conn.commit()
         except Exception as error:
-            logging.debug(error)
-            print(error)
+            log_message(error, show=True)
         return results
 
-    def get_price_refresh_sql(self, max_tickers, lookback_window):
-        sql = '''
-            SELECT REPLACE(c.Ticker,'.','-') AS Ticker
-            FROM companies c
-            WHERE NOT EXISTS 
-            (
-                SELECT 1
-                FROM companies c2
-                    INNER JOIN prices p ON c2.CompanyID = p.CompanyID
-                WHERE c2.CompanyID = c.CompanyID
-                    AND p.Date > date_sub(now(), interval ''' + str(lookback_window) + ''' day)
-                LIMIT 1
-            )
-            ORDER BY Ticker
-            LIMIT ''' + str(max_tickers) + ''';'''
-        logging.debug('SQL for Price Refresh: ' + sql)
-        return sql
-
-    def get_div_refresh_sql(self, max_tickers, lookback_window):
-        sql = '''
-            SELECT REPLACE(c.Ticker,'.','-') AS Ticker
-            FROM companies c
-            WHERE c.HasDividend = 1
-                AND NOT EXISTS 
-                (
-                    SELECT 1
-                    FROM companies c2
-                        INNER JOIN dividends d ON c2.CompanyID = d.CompanyID
-                    WHERE c2.CompanyID = c.CompanyID
-                        AND d.ExDivDate > date_sub(now(), interval ''' + str(lookback_window) + ''' day)
-                    LIMIT 1
-                )
-            ORDER BY Ticker
-            LIMIT ''' + str(max_tickers) + ''';'''
-        logging.debug('SQL for Dividends Refresh: ' + sql)
-        return sql
+    def get_sandp_companies_list(self):
+        try:
+            context = ssl._create_unverified_context()
+            response = req.urlopen(url=self.companies_data_url, context=context)
+            html = response.read()
+            sandp_companies = pd.read_html(html)
+            sp_curr = sandp_companies[0]
+            log_df_details('S&P Current Company List', sp_curr)
+            self.load_df_to_stg(sp_curr, '_stg_sp_current')
+            sp_hist = sandp_companies[1]
+            log_df_details('S&P Historical Company List', sp_hist)
+            self.load_df_to_stg(sp_hist, '_stg_sp_history')
+            self.call_proc('sp_refresh_companies')   
+        except Exception as error:
+            log_message(error, show=True)         
 
     def get_ticker_list(self, sql):
         try:
@@ -136,39 +135,37 @@ class Pipeline:
             cursor.close()
             self.db_conn.commit()     
         except Exception as error:
-            logging.debug(error)
-            print(error)
-        logging.debug(df_tickers)
+            log_message(error, show=True) 
+        log_df_details('Tickers', df_tickers)
         return df_tickers        
 
     def refresh_prices(self, max_tickers, lookback_window):
         try:
-            sql = self.get_price_refresh_sql(max_tickers, lookback_window)
-            logging.debug('List of Tickers for Price Refresh:')
+            sql = get_price_refresh_sql(max_tickers, lookback_window)
+            log_message('List of Tickers for Price Refresh:')
             tickers = self.get_ticker_list(sql)        
             for _, row in tickers.iterrows():
                 ticker = str(row['Ticker'])
-                logging.debug('Get the price history for: ' + ticker)
+                log_message('Get the price history for: ' + ticker, show=True)
                 yf_ticker = yf.Ticker(ticker)
                 df_price_hist = yf_ticker.history(period='40y')
                 if df_price_hist.empty == False:
-                    self.log_df_details(ticker + ' Price History', df_price_hist)
-                    self.load_df_to_stg(df=df_price_hist, stg_tbl_name='_stg_price_hist') 
-                    df_div = DataFrame(yf_ticker.dividends)
+                    log_df_details(ticker + ' Price History', df_price_hist)
+                    self.load_df_to_stg(df_price_hist, '_stg_price_hist') 
+                    df_div = pd.DataFrame(yf_ticker.dividends)
                     if df_div.empty == False:
-                        self.log_df_details(ticker + ' Dividends', df_div)
-                        self.load_df_to_stg(df=df_div, stg_tbl_name='_stg_has_div') # for updating company.HasDividend column
-                    self.call_proc('sp_refresh_prices_company_data',args=[ticker,])
+                        log_df_details(ticker + ' Dividends', df_div)
+                        self.load_df_to_stg(df_div, '_stg_has_div') # for updating company.HasDividend column
+                    self.call_proc('sp_refresh_prices_company_data', args=[ticker,])
+            log_message('Prices refreshed successfully', show=True)
         except Exception as error:
-            logging.debug(error)
-            print(error)
+            log_message(error, show=True) 
 
     def refresh_dividends(self, max_tickers, lookback_window):
         try:
-            sql = self.get_div_refresh_sql(max_tickers, lookback_window)
-            logging.debug('List of Tickers for Dividend Refresh:')
+            sql = get_div_refresh_sql(max_tickers, lookback_window)
+            log_message('List of Tickers for Dividend Refresh:')
             tickers = self.get_ticker_list(sql)  
-            url = self.div_data_url
             querystring = {
                 'symbol':'<ticker>',
                 'years':'40',
@@ -180,18 +177,18 @@ class Pipeline:
             }
             for _, row in tickers.iterrows():
                 ticker = str(row['Ticker'])
-                logging.debug('Get the dividend history for: ' + ticker)
+                log_message('Get the dividend history for: ' + ticker, show=True)
                 querystring['symbol']=ticker
-                response = requests.request('GET', url, headers=headers, params=querystring)
+                response = requests.request('GET', url=self.div_data_url, headers=headers, params=querystring)
                 json_data = json.loads(response.text)
                 df_div_hist = pd.json_normalize(json_data, record_path =['data'])
                 if df_div_hist.empty == False:
-                    self.log_df_details(ticker + ' Full Dividend History', df_div_hist)
-                    self.load_df_to_stg(df=df_div_hist, stg_tbl_name='_stg_div_hist') 
-                    self.call_proc('sp_refresh_dividends',args=[ticker,])
+                    log_df_details(ticker + ' Full Dividend History', df_div_hist)
+                    self.load_df_to_stg(df_div_hist, '_stg_div_hist') 
+                    self.call_proc('sp_refresh_dividends', args=[ticker,])
+            log_message('Dividends refreshed successfully', show=True)
         except Exception as error:
-            logging.debug(error)
-            print(error)
+            log_message(error, show=True) 
 
     def close_db_conn(self):
         self.db_conn.close()
@@ -232,10 +229,10 @@ def run_full_pipline():
     pipeline1.get_sandp_companies_list()
 
     # refresh price history data
-    pipeline1.refresh_prices(max_tickers=25, lookback_window=0)
+    pipeline1.refresh_prices(max_tickers=10, lookback_window=0)
 
     # refresh dividend history
-    pipeline1.refresh_dividends(max_tickers=5, lookback_window=50)
+    pipeline1.refresh_dividends(max_tickers=2, lookback_window=50)
 
     # close db connection at end of run
     pipeline1.close_db_conn()
